d76e6f0dd1c9e8e46d4a4161e5005ebea7e9133b
Isak Karin <isak.karin00@gmail.com>
2025-01-29 17:44:03 +0100

    visual

--- Amalgam/src/SDK/Helpers/Fonts/Fonts.cpp
+++ Amalgam/src/SDK/Helpers/Fonts/Fonts.cpp
@@ -4,7 +4,7 @@
 
 void CFonts::Reload(float flDPI)
 {
-	m_mapFonts[FONT_ESP] = { "Verdana", int(12.f * flDPI), FONTFLAG_OUTLINE, 0 };
+	m_mapFonts[FONT_ESP] = { "Verdana", int(12.f * flDPI), FONTFLAG_DROPSHADOW, 0 };
 	m_mapFonts[FONT_INDICATORS] = { "micross", int(13.f * flDPI), FONTFLAG_OUTLINE, 0 };
 
 	for (auto& [_, fFont] : m_mapFonts)
